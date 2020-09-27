4e6d043d53149183ee9841ab14554f86d77460da
Andrzej Broński <andrzej1_1@o2.pl>
2020-09-27 10:47:14 +0200

    Add possibility to pass 'none' as provider

--- main.ts
+++ main.ts
@@ -6,6 +6,12 @@ import { RedisClient } from 'redis';
 import * as child_process from 'child_process';
 
 function runAutoscaler(providerName: string, rcl: RedisClient, wflib: object, plugins: any[]) {
+  /* Skip autoscaler if specified 'none'. */
+  if (providerName == "none") {
+    Loggers.base.debug("[main] Not running autoscaler process, because 'none' provider is specified.");
+    return;
+  }
+
   /* Create API, engine process and bind it via RPC. */
   let api = new API(rcl, wflib, plugins);
   Loggers.base.debug("[main] Running process with engine.js");
