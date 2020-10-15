45aeed2c0813fe9ae969f0af704ded11ccc0252c
Andrzej Broński <andrzej1_1@o2.pl>
2020-10-15 14:00:35 +0200

    Now memoryStringToBytes support all possible formats

--- utils.ts
+++ utils.ts
@@ -3,26 +3,97 @@ import Loggers from './logger';
 class Utils {
   static memoryStringToBytes(mem: string): number | Error  {
     Loggers.base.silly('[Utils] memoryStringToBytes(' + mem + ')');
-    let val: number = parseInt(mem, 10);
-    let suffix: string = mem.substring(mem.length - 2);
-    let power: number = 0;
-    switch (suffix) {
-      case "Ki":
-        power = 10;
-        break;
-      case "Mi":
-        power = 20;
-        break;
-      case "Gi":
-        power = 30;
-        break;
-      case "Ti":
-        power = 40;
-        break;
-      default:
-        return Error("Unsupported suffix '" + suffix + "'");
+    let format1 = mem.match(/^(?<num>\d+)$/);
+    if (format1 != null) {
+      let numPart = format1?.groups?.num;
+      if (numPart == undefined) {
+        throw Error('Fatal error');
+      }
+      let val: number = parseInt(numPart, 10);
+      return val;
     }
-    return val * Math.pow(2, power);
+
+    let format2 = mem.match(/^(?<base>\d+)e(?<exponent>\d+)$/);
+    if (format2 != null) {
+      let basePart = format2?.groups?.base;
+      let exponentPart = format2?.groups?.exponent;
+      if (basePart == undefined || exponentPart == undefined) {
+        throw Error('Fatal error');
+      }
+      let baseVal: number = parseInt(basePart, 10);
+      let exponentVal: number = parseInt(exponentPart, 10);
+      return baseVal * Math.pow(10, exponentVal);;
+    }
+
+    let format3 = mem.match(/^(?<num>\d+)(?<unit>[EPTHMK])$/);
+    if (format3 != null) {
+      let numPart = format3?.groups?.num;
+      let unitPart = format3?.groups?.unit;
+      if (numPart == undefined || unitPart == undefined) {
+        throw Error('Fatal error');
+      }
+      let baseVal: number = parseInt(numPart, 10);
+      let power: number;
+      switch (unitPart) {
+        case "E":
+          power = 18;
+          break;
+        case "P":
+          power = 15;
+          break;
+        case "T":
+          power = 12;
+          break;
+        case "G":
+          power = 9;
+          break;
+        case "M":
+          power = 6;
+          break;
+        case "K":
+          power = 3;
+          break;
+        default:
+        throw Error('Fatal error');
+      }
+      return baseVal * Math.pow(10, power);;
+    }
+
+    let format4 = mem.match(/^(?<num>\d+)(?<unit>[EPTHMK]i)$/);
+    if (format4 != null) {
+      let numPart = format4?.groups?.num;
+      let unitPart = format4?.groups?.unit;
+      if (numPart == undefined || unitPart == undefined) {
+        throw Error('Fatal error');
+      }
+      let baseVal: number = parseInt(numPart, 10);
+      let power: number;
+      switch (unitPart) {
+        case "Ei":
+          power = 60;
+          break;
+        case "Pi":
+          power = 50;
+          break;
+        case "Ti":
+          power = 40;
+          break;
+        case "Gi":
+          power = 30;
+          break;
+        case "Mi":
+          power = 20;
+          break;
+        case "Ki":
+          power = 10;
+          break;
+        default:
+        throw Error('Fatal error');
+      }
+      return baseVal * Math.pow(2, power);;
+    }
+    
+    return Error("[Utils] Unknown format");
   }
 
   static cpuStringToMillis(cpu: string): number | Error {
