1e513b5f6317dbaa1eb6d8e5f47e78482fcd5200
Andrzej Broński <andrzej1_1@o2.pl>
2020-10-19 21:57:22 +0200

    Add ScalingDecision class (simple storage)

---
+++ src/hyperflow/workflow/scalingDecision.ts
@@ -0,0 +1,28 @@
+type timestamp = number;
+
+class ScalingDecision
+{
+  private machinesDiff: number;
+  private time: timestamp;
+
+  public constructor(machinesDiff: number, time: timestamp) {
+    this.machinesDiff = machinesDiff;
+    this.time = time;
+  }
+
+  /**
+   * Getter for machinesDiff.
+   */
+  public getMachinesDiff(): number {
+    return this.machinesDiff;
+  }
+
+  /**
+   * Getter for time.
+   */
+  public getTime(): timestamp {
+    return this.time;
+  }
+}
+
+export default ScalingDecision;
