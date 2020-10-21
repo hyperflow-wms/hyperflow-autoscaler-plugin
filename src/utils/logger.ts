bd844c076351163c9bb1a84ea2a359687b0b2203
Andrzej Broński <andrzej1_1@o2.pl>
2020-10-21 12:25:51 +0200

    [WIP 1/2] Refactoring logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,8 +2,13 @@ import winston = require('winston');
 import path = require('path');
 import { format } from 'winston';
 let { combine, timestamp,  printf } = format;
+import stackTrace = require('stack-trace');
 
 const logDir = path.join(__dirname, '..', 'logs');
+
+/*************
+ * BASE LOGS *
+ *************/
 
 const myBaseLogFormat = combine(
   timestamp(),
@@ -12,17 +17,52 @@ const myBaseLogFormat = combine(
   })
 );
 
-const baseLogger = winston.loggers.add('runtime', {
-  level: 'info',
-  format: myBaseLogFormat,
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    new winston.transports.File({ filename: path.join(logDir, 'error.log'), options: {}, level: 'error' }),
-    new winston.transports.File({ filename: path.join(logDir, 'debug.log'), options: {}, level: 'debug' }),
-    new winston.transports.File({ filename: path.join(logDir, 'silly.log'), options: {}, level: 'silly' }),
-    new winston.transports.Console({ level: 'info' }),
-  ],
-});
+const commonFileDebugTransport = new winston.transports.File({ filename: path.join(logDir, 'debug.log'), options: {}, level: 'debug' });
+const commonFileErrorTransport = new winston.transports.File({ filename: path.join(logDir, 'error.log'), options: {}, level: 'error' });
+const commonFileSillyTransport =  new winston.transports.File({ filename: path.join(logDir, 'silly.log'), options: {}, level: 'silly' });
+const commonConsoleInfoTransport = new winston.transports.Console({ level: 'info' });
+
+let baseLoggers = new Map<string, winston.Logger>();
+
+function getBaseLogger(name?: string): winston.Logger {
+  /* Fallback to parent module name. */
+  if (name === undefined) {
+    let callerFilename = stackTrace.get()[1].getFileName();
+    name = path.basename(callerFilename, path.extname(callerFilename));
+  }
+
+  /* Get existing logger. */
+  if (baseLoggers.has(name) === true) {
+    // @ts-ignore: Object is possibly 'undefined'.
+    let logger: winston.Logger = baseLoggers.get(name);
+    return logger;
+  }
+
+  /* Make new logger. */
+  const logName = '_' + name + '.log';
+  const newFileTransport = new winston.transports.File({ filename: path.join(logDir, logName), options: {}, level: 'silly' });
+  const newBaseLogger = winston.loggers.add(name, {
+    level: 'info',
+    format: myBaseLogFormat,
+    defaultMeta: { service: 'user-service' },
+    transports: [
+      commonFileDebugTransport,
+      commonFileErrorTransport,
+      commonFileSillyTransport,
+      commonConsoleInfoTransport,
+      newFileTransport
+    ],
+  });
+
+  baseLoggers.set(name, newBaseLogger);
+  return newBaseLogger;
+}
+
+export { getBaseLogger };
+
+/***********************************
+ * ADDITIONAL LOGS (SCALING CHART) *
+ ***********************************/
 
 const scalingLogFormat = combine(
   timestamp(),
@@ -43,7 +83,4 @@ const scalingLogger = winston.loggers.add('scaling', {
   ],
 });
 
-export default {
-  'base': baseLogger,
-  'scaling': scalingLogger,
-};
+export { scalingLogger };
